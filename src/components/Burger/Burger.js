94c5a03d6cd68db44e7d6848259480651107347f
Artur Dąbrowski <artur@lifeinmobile.com>
2018-07-16 15:02:11 +0200

    monday

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,6 +4,15 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 
 const burger = (props) => {
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])]
+                .map((_,i) => {
+                 <BurgerIngredient key={igKey + i} type={igKey}/>
+            })
+        });
+    console.log(transformedIngredients);
+    
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,10 +3,18 @@ import Aux from '../../hoc/Auxx';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 2,
+            cheese: 2,
+            meet: 3
+        }
+    }
     render(){
         return(
             <Aux>
-                <Burger/>
+                <Burger ingredients={this.state.ingredients}/>
                 <div>Build Controls</div>
             </Aux>
         );
